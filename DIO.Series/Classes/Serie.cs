3cc30b14fbb301c3e335a654939d0c2021717c44
Ana Rizzi <ana.rizzi.eu@gmail.com>
2021-02-24 15:30:08 -0300

    Atividade de CRUD de séries

---
+++ DIO.Series/Classes/Serie.cs
@@ -0,0 +1,55 @@
+﻿using DIO.Series.Enum;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero;
+            retorno += "\nTítulo: " + this.Titulo;
+            retorno += "\nDescrição: " + this.Descricao;
+            retorno += "\nAno de Início: " + this.Ano;
+            retorno += "\nExcluído: " + this.Excluido;
+
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+    }
+}
---
+++ DIO.Series/Enum/Genero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Series.Enum
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Fantasia = 4,
+        Suspense = 5,
+        Terror = 6
+    }
+}
